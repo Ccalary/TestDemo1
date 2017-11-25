15897397d2df52cee2a12ad07a27a503bae15758
c_calary <229336009@qq.com>
2017-11-25 23:20:26 +0800

    监听runloop(未完成)

---
+++ TestDemo1/MainClass/MainClass/Mine/FuncDisplay/GoodTableView/ImageTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  ImageTableViewCell.h
+//  TestDemo1
+//
+//  Created by caohouhong on 2017/11/21.
+//  Copyright © 2017年 caohouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageTableViewCell : UITableViewCell
+@property (nonatomic, strong) UILabel *titleLabel;
+@property (nonatomic, strong) UIImageView *imageView1, *imageView2, *imageView3;
+@end
